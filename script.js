dbe2bd565f6c32ea04e071f5b26105ea73b7994e
winqdev <103835411+winqdev@users.noreply.github.com>
2023-05-07 12:34:23 +0300

    Uploaded xd

---
+++ script.js
@@ -0,0 +1,56 @@
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+document.addEventListener("DOMContentLoaded", async function(event) {
+  var loader = document.getElementById("loading-animation");
+  var body = document.getElementById("bodyid");
+  var discord = document.getElementById("discord");
+  var itch = document.getElementById("itch");
+
+    loader.style.animation = "rotate 1s infinite";
+    body.style.backgroundColor = "black";
+    body.style.backgroundImage = "none";
+    discord.style.display = "none";
+    itch.style.display = "none";
+    setTimeout(function() {
+      body.style.opacity = 1;
+      loader.style.animation = "none";
+      body.style.backgroundImage = "url('/assets/background.png')";
+      discord.style.display = "inline-block";
+      itch.style.display = "inline-block";
+      textstart();
+    }, 2500);
+
+});
+
+async function textstart() {
+  var g = 0;
+  var txtw = 'Voices of The Void';
+  var speedy = 50;
+
+  var i = 0;
+  var txt = '(unofficial website)';
+  var speed = 50;
+
+  function typeWriter() {
+    if (i < txt.length) {
+      document.getElementById("demo").innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    } else {
+      // call load function when typewriter animation is complete;
+    }
+  }
+
+
+  function loadtexto() {
+    if (g < txtw.length) {
+      document.getElementById("datitle").innerHTML += txtw.charAt(g);
+      g++;
+      setTimeout(loadtexto, speedy);
+    }
+  }
+
+  loadtexto();
+  await delay(1100);
+  typeWriter();
+}
